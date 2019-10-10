5d3f0b68f7096af891515960ddb98fa05e5b252d
Dean L Betty <deanlloyd64@gmail.com>
2019-10-10 19:20:15 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -10,24 +10,24 @@ INSERT INTO books (title, year, series_id) VALUES
   ("A Clash of Kings", 1998, 1),
   ("A storm of Swords", 2000, 1);
   
-INSERT INTO subgenres (name) VALUES 
-  ("Fantasy"),
-  ("Science Fiction");
+-- INSERT INTO subgenres (name) VALUES 
+--   ("Fantasy"),
+--   ("Science Fiction");
   
-INSERT INTO authors (name) VALUES 
-  ("George R.R. Martin"),
-  ("Suzanne Collins");
+-- INSERT INTO authors (name) VALUES 
+--   ("George R.R. Martin"),
+--   ("Suzanne Collins");
   
 
-INSERT INTO characters (name, motto, species, author_id) VALUES 
-  ("Katniss Everdeen", "I volunteer as tribute", "Human", 2),
-  ("Peeta Mellark", "I make-a da bread", "Human", 2),
-  ("Gale Hawthorne", "The capitol is terrible", "Human", 2),
-  ("Primrose Everdeen", "I die in the second book", "Human", 2),
-  ("Jon Snow", "My watch is over.", "Human", 1),
-  ("Ned Stark", "I die in the first season", "Human", 1),
-  ("Daenerys Targaryen", "My character development goes down the drain", "Human", 1),
-  ("Petyr Baelish", "Chaos is a ladder", "Human", 1);
+-- INSERT INTO characters (name, motto, species, author_id) VALUES 
+--   ("Katniss Everdeen", "I volunteer as tribute", "Human", 2),
+--   ("Peeta Mellark", "I make-a da bread", "Human", 2),
+--   ("Gale Hawthorne", "The capitol is terrible", "Human", 2),
+--   ("Primrose Everdeen", "I die in the second book", "Human", 2),
+--   ("Jon Snow", "My watch is over.", "Human", 1),
+--   ("Ned Stark", "I die in the first season", "Human", 1),
+--   ("Daenerys Targaryen", "My character development goes down the drain", "Human", 1),
+--   ("Petyr Baelish", "Chaos is a ladder", "Human", 1);
   
-INSERT INTO character_books (book_id, character_id) VALUES 
-  ();
+-- INSERT INTO character_books (book_id, character_id) VALUES 
+--   ();
