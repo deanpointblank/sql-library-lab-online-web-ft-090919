4e3c0d0ff7aa83e7409a98e6ea637df4b5e05e9b
Dean L Betty <deanlloyd64@gmail.com>
2019-10-10 21:18:35 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -46,7 +46,7 @@ def select_series_title_with_most_human_characters
   JOIN  authors
     ON series.author_id = authors.id
   JOIN authors
-    ON authors.
+    ON authors.id = .author_id
   SQL
 end
 
