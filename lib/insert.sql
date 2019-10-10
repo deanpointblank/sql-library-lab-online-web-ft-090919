4c9c445b93922bcc870238e05409bcb01bf299cb
Dean L Betty <deanlloyd64@gmail.com>
2019-10-10 18:52:04 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,20 @@
-INSERT INTO series (title, author_id, subgenre_id) VALUES ("");
-INSERT INTO sungenres (name) VALUES ();
-INSERT INTO authors (name) VALUES ();
-INSERT INTO books (title, year, series_id) VALUES ():
-INSERT INTO characters (name, motto, species, author_id)
-INSERT
-
-
-
-
-
-
-
-
-
+INSERT INTO series (title, author_id, subgenre_id) VALUES 
+  ("Game of Thrones", 1, 1),
+  ("The Hunger Games", 2, 2);
+  
+INSERT INTO subgenres (name) VALUES 
+  ("Fantasy"),
+  ("Young Adult");
+  
+INSERT INTO authors (name) VALUES 
+  ("George R.R. Martin"),
+  ("Susan Collins");
+  
+INSERT INTO books (title, year, series_id) VALUES 
+  ();
+  
+INSERT INTO characters (name, motto, species, author_id) VALUES 
+  ();
+  
+INSERT INTO character_books (book_id, character_id) VALUES 
+  ();
