020c04b7d077372ea777adccad4d1df2e6827430
Dean L Betty <deanlloyd64@gmail.com>
2019-10-10 19:19:27 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,13 +2,13 @@ INSERT INTO series (title, author_id, subgenre_id) VALUES
   ("A Song of Ice and Fire", 1, 1),
   ("The Hunger Games", 2, 2);
   
-INSERT INTO books (title, year, series_id) VALUES 
-  ("The Hunger Games", 2008, 2),
-  ("Catching Fire", 2009, 2),
-  ("Mockingjay", 2010, 2),
-  ("A Game of Thrones", 1996, 1,),
-  ("A Clash of Kings", 1998, 1),
-  ("A storm of Swords", 2000, 1);
+-- INSERT INTO books (title, year, series_id) VALUES 
+--   ("The Hunger Games", 2008, 2),
+--   ("Catching Fire", 2009, 2),
+--   ("Mockingjay", 2010, 2),
+--   ("A Game of Thrones", 1996, 1,),
+--   ("A Clash of Kings", 1998, 1),
+--   ("A storm of Swords", 2000, 1);
   
 -- INSERT INTO subgenres (name) VALUES 
 --   ("Fantasy"),
