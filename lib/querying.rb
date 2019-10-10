c3832405d45529141ef1da38dbbdc06838771011
Dean L Betty <deanlloyd64@gmail.com>
2019-10-10 20:08:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,8 +1,9 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   <<-SQL
-  SELECT books.titles, books.years
+  SELECT books.titles, books.year
   FROM books
-  WHERE se
+  WHERE books.series_id = 1
+  ORDER BY books.year
   SQL
 end
 
